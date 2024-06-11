a8d627ce3bdd87792757f9339364c16b6accad10
Carolinii <carolinii.contact@gmail.com>
2024-06-11 14:00:31 +0200

    update main.cpp file and move SFML lib to game_skeleton directory

--- main.cpp
+++ main.cpp
@@ -1,23 +1,15 @@
-#include <SFML/Graphics.hpp>
+#include "Game.hpp"
 
 int main()
 {
-    sf::RenderWindow window( sf::VideoMode(200, 200), "SFML works!" );
-    sf::CircleShape shape( 100.f );
-    shape.setFillColor( sf::Color::Green );
+    Game game;
 
-    while( window.isOpen() )
+    while( !game.getWindow()->isDone() )
     {
-        sf::Event event{};
-        while( window.pollEvent( event ) )
-        {
-            if( event.type == sf::Event::Closed )
-                window.close();
-        }
-
-        window.clear();
-        window.draw( shape );
-        window.display();
+        game.handleInput();
+        game.update();
+        game.render();
+        game.restartClock();
     }
 
     return 0;
